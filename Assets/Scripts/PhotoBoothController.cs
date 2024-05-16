8b0dfb4348a4b7cc44fb0abc65cb3b0b9e5df81d
limicGGGamer <limiclee@gggamer.org>
2024-05-16 16:35:02 +0800

    updated

---
+++ Assets/Scripts/PhotoBoothController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotoBoothController : MonoBehaviour
+{
+    public PhotoBooth_WebcamTool webcamTool;
+    public PhotoBooth_PhotoTool photoTool;
+    public PhotoAnimController photoAnimController;
+    
+    public string savePath;
+    private string fileName;
+    private int photoCount;
+    
+    [Header("UI")]
+    public Button btn_takePhoto;
+    public RawImage rImg_takenPhoto;
+    
+    void Start () {
+
+        webcamTool.Init();
+        photoTool.Init();
+        photoAnimController.Init();
+
+        rImg_takenPhoto.gameObject.SetActive(false);
+    }
+
+    void OnEnable()
+    {
+        btn_takePhoto.onClick.AddListener(HandleBtnTakePhotoClicked);
+        PhotoAnimController.OnCountDownFinish += HandleCountDownFinish;
+        PhotoTool.OnPhotoTaken += HandlePhotoTaken;
+    }
+
+    void OnDisable()
+    {
+        btn_takePhoto.onClick.RemoveAllListeners();
+        PhotoAnimController.OnCountDownFinish -= HandleCountDownFinish;
+        PhotoTool.OnPhotoTaken -= HandlePhotoTaken;
+    }
+
+    void HandleBtnTakePhotoClicked()
+    {
+        // Hide the button
+        btn_takePhoto.transform.parent.gameObject.SetActive(false);
+        // Play count down animation
+        photoAnimController.PlayCountDownAnim();
+    }
+
+    void HandleCountDownFinish()
+    {
+        Debug.Log("Count Down Finish!");
+        photoTool.TakePhoto();
+    }
+
+    void HandleShotFinish()
+    {
+        Debug.Log("Shot Finish!");
+    }
+
+    void HandlePhotoTaken(Texture2D _takenPhoto)
+    {
+        photoAnimController.PlayShotAnim();
+
+        photoCount += 1;
+
+        // Show rImg_takenPhoto
+        rImg_takenPhoto.texture = _takenPhoto;
+        rImg_takenPhoto.gameObject.SetActive(true);
+
+#if UNITY_EDITOR
+
+        // Save taken photo as a png file
+        string fullName = FileIOUtility.GenerateFileName(fileName, photoCount, FileIOUtility.FileExtension.PNG);
+        FileIOUtility.SaveImage(_takenPhoto, savePath, fullName, FileIOUtility.FileExtension.PNG);
+#else
+        fileName = DateTime.Now.ToString(@"yyyy\-MM\-dd\-hh\-mm\-ss");
+        NativeGallery.Permission permission =
+            NativeGallery.SaveImageToGallery(_takenPhoto, "GGGamer_PhotoBooth", fileName);
+        Debug.Log("Permission result: "+permission);
+        SharePhoto(_takenPhoto);
+#endif
+        // Wait for some time and go back to a playable status
+        StartCoroutine(E_WaitAndReset());
+    }
+
+    IEnumerator E_WaitAndReset()
+    {
+        yield return new WaitForSeconds(2f);
+
+        // Show the button again
+        btn_takePhoto.transform.parent.gameObject.SetActive(true);
+
+        // Hide rImg_takenPhoto
+        rImg_takenPhoto.gameObject.SetActive(false);
+    }
+
+    void SharePhoto(Texture2D _takenPhoto)
+    {
+        new NativeShare().AddFile(_takenPhoto).SetSubject("Print Image").SetText("Printing Image...").Share();
+    }
+}
---
+++ Assets/Scripts/PhotoBooth_PhotoTool.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoBooth_PhotoTool : PhotoTool
+{
+    public override void Init()
+    {
+        (width, height) = (Screen.width, Screen.height);
+        base.Init();
+    }
+}
